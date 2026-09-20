605b7cf53d921c0992cdaa2cc9015b7395f29139
ZTS439 <eigaming520@gmail.com>
2026-09-20 00:45:03 -0500

    bench 502762

--- src/search/params.rs
+++ src/search/params.rs
@@ -156,7 +156,8 @@ params! {
     ndp_depth: i32 => 8;
 
     mp_quiet_neutral_malus: i32 => 5000;
-    mp_quiet_blocks_null_best_bonus: i32 => 100;
+    mp_quiet_blocks_null_best_bonus: i32 => 400;
+    mp_loud_blocks_null_best_bonus: i32 => 20;
 
     qsldp_threshold: i32 => 2;
     qsdcp_threshold: i32 => 2;
